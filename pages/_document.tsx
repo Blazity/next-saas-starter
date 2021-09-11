18d5d874d61542b8b7707f8b6585ef1a9f83db7c
Bart Stefanski <contact@bstefanski.com>
2021-09-12 00:40:42 +0200

    setup styled-components, eslint, etc

---
+++ pages/_document.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App: any) => (props) => sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body className="light-theme">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
