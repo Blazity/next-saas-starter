d59f4da9fe29a77e695542392afebe433f8fcae3
Igor Lasota <igorlasota07@gmail.com>
2021-09-29 14:26:47 +0200

    create footer lists

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,52 @@ import styled from 'styled-components';
 
 export default function Footer () {
     return (
-        <div>
-        </div>
+        <ListContainer>
+            <List>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+            </List>
+            <List>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+                </List>
+            <List>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+             </List>
+            <List>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+            </List>
+            <List>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+            </List>
+        </ListContainer>
     )
 }
+
+const ListContainer = styled.div`
+    display: flex;    
+    flex-direction: row;
+`
+
+const List = styled.ul`
+    flex-basis: 20%;
+    list-style-type: none;
+`
