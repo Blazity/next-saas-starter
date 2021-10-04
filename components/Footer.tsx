163be997cb66e3b5fae5a998b29dd0edffaae48d
Igor Lasota <igorlasota07@gmail.com>
2021-10-04 13:08:29 +0200

    change the way footer is build

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,45 +1,52 @@
 import styled from 'styled-components';
+import { FooterItems, SingleFooterList, SingleFooterListItem } from 'types';
+import NextLink from 'next/link';
 
 
 export default function Footer () {
+    const footerItems: FooterItems = [
+        {
+            header: 'Company',
+            listItem: [
+                {title: 'About' , href: 'https://www.youtube.com/watch?v=T78nq62aQgM'},
+                {title: 'News' , href: 'https://www.youtube.com/watch?v=T78nq62aQgM'}
+            ]
+        },
+        {
+            header: 'Product',
+            listItem: [
+                {title: 'Media Database' , href: 'https://www.youtube.com/watch?v=T78nq62aQgM'},
+                {title: 'Press Release Creator' , href: 'https://www.youtube.com/watch?v=T78nq62aQgM'},
+                {title: 'Online Newsroom' , href: 'https://www.youtube.com/watch?v=T78nq62aQgM'},
+                {title: 'Pricing' , href: 'https://www.youtube.com/watch?v=T78nq62aQgM'}
+            ]
+        },
+        {
+            header: 'Knowledge',
+            listItem: [
+                {title: 'Product Updates' , href: 'https://www.youtube.com/watch?v=T78nq62aQgM'},
+                {title: 'Blog' , href: 'https://www.youtube.com/watch?v=T78nq62aQgM'},
+                {title: 'Help Center' , href: 'https://www.youtube.com/watch?v=T78nq62aQgM'}
+            ]
+        },
+        {
+            header: 'Legal',
+            listItem: [
+                {title: 'Privacy Policy' , href: 'https://www.youtube.com/watch?v=T78nq62aQgM'},
+                {title: 'Terms of Service' , href: 'https://www.youtube.com/watch?v=T78nq62aQgM'},
+                {title: 'Billing Policy' , href: 'https://www.youtube.com/watch?v=T78nq62aQgM'},
+                {title: 'Data Procesing Clause' , href: 'https://www.youtube.com/watch?v=T78nq62aQgM'},
+                {title: 'Brand Policy' , href: 'https://www.youtube.com/watch?v=T78nq62aQgM'}
+            ]
+        },
+    ]
+
     return (
         <div>
             <ListContainer>
-                <List>
-                    <ListHeader>lorem</ListHeader>
-                    <ListItem>ipsum</ListItem>
-                    <ListItem>ipsum</ListItem>
-                    <ListItem>ipsum</ListItem>
-                    <ListItem>ipsum</ListItem>
-                </List>
-                <List>
-                    <ListHeader>lorem</ListHeader>
-                    <ListItem>ipsum</ListItem>
-                    <ListItem>ipsum</ListItem>
-                    <ListItem>ipsum</ListItem>
-                    <ListItem>ipsum</ListItem>
-                </List>
-                <List>
-                    <ListHeader>lorem</ListHeader>
-                    <ListItem>ipsum</ListItem>
-                    <ListItem>ipsum</ListItem>
-                    <ListItem>ipsum</ListItem>
-                    <ListItem>ipsum</ListItem>
-                </List>
-                <List>
-                    <ListHeader>lorem</ListHeader>
-                    <ListItem>ipsum</ListItem>
-                    <ListItem>ipsum</ListItem>
-                    <ListItem>ipsum</ListItem>
-                    <ListItem>ipsum</ListItem>
-                </List>
-                <List>
-                    <ListHeader>lorem</ListHeader>
-                    <ListItem>ipsum</ListItem>
-                    <ListItem>ipsum</ListItem>
-                    <ListItem>ipsum</ListItem>
-                    <ListItem>ipsum</ListItem>
-                </List>
+            {footerItems.map((singleItem) => (
+                <FooterList key={singleItem.header} {...singleItem} />
+            ))}
             </ListContainer>
             <FooterBar>
                 <p>&copy; Copyright 2021</p>
@@ -48,24 +55,51 @@ export default function Footer () {
     )
 }
 
+
+function FooterList({header, listItem}: SingleFooterList){
+    return(
+        <div>
+            <ListHeader>{header}</ListHeader>
+            {listItem.map((singleItem) => (
+                <ListItem key={singleItem.href} {...singleItem} />
+          ))}
+        </div>
+    )
+}
+
+function ListItem({ title, href }: SingleFooterListItem) {
+    return(
+        <ListItemWrapper>
+            <NextLink href={href} passHref>
+                <a>{title}</a>
+            </NextLink>
+        </ListItemWrapper>
+    )
+}
+
 const ListContainer = styled.div`
     display: flex;    
     flex-direction: row;
     justify-content: space-evenly;
+    @media (max-width: 768px) {
+        flex-direction: column;
+        padding: 2.5rem;
+      }
 `
 
-const List = styled.ul`
-    list-style-type: none;
-`
-
-const ListHeader = styled.li`
+const ListHeader = styled.p`
     font-weight: bold;
-    font-size: 1.5rem;
+    font-size: 2rem;
 `
-const ListItem = styled.li`
-    font-size: 1.3rem;
+const ListItemWrapper = styled.p`
+    font-size: 1.5rem;
     padding-top: 0.5rem;
     padding-bottom: 0.75rem;
+    
+    a {
+        text-decoration: none;
+        color: #696969;
+    }
 `
 const FooterBar = styled.div`
     padding: 1.5rem;
--- types.ts
+++ types.ts
@@ -1,2 +1,6 @@
 export type SingleNavItem = { title: string; href: string; outlined?: boolean }
 export type NavItems = SingleNavItem[]
+export type SingleFooterListItem = { title: string; href: string; }
+export type FooterListItems = SingleFooterListItem[]
+export type SingleFooterList = { header: string; listItem: FooterListItems; }
+export type FooterItems = SingleFooterList[]
