bcee42a1444dfe1393ccf1101a14b05a05621d4d
bhaskarandmr <99174239+bhaskarandmr@users.noreply.github.com>
2022-09-10 10:31:51 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -68,7 +68,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         >
           <Component {...pageProps} />
         </TinaEditProvider>
-        <WaveCta />
         <Footer />
       </Providers>
     </>
