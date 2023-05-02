e2136b45acf7acebc18097f035979824415ab862
reidelkins <reidelkins@outlook.com>
2023-05-02 15:25:00 -0500

    make needed addition

--- views/PricingPage/PricingTablesSection.tsx
+++ views/PricingPage/PricingTablesSection.tsx
@@ -1,45 +1,107 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import ArrowButton from 'components/ArrowButton';
 import AutofitGrid from 'components/AutofitGrid';
 import PricingCard from 'components/PricingCard';
 import SectionTitle from 'components/SectionTitle';
 
 export default function PricingTablesSection() {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const pricingPlans = [
+    {
+      title: 'Free',
+      description: 'Give us a try for free',
+      price: '$0',
+      benefits: [
+        '1 User',
+        'See How Many Customers You Have Moving',
+        'See All Your Customers That Have Recently Moved',
+        'Get a New Leads List For People Moving In Your Area',
+      ],
+      checkoutLink: '',
+    },
+    {
+      title: 'Silver',
+      description: 'For Small Businesses',
+      price: '$150',
+      benefits: [
+        '5,000 Clients',
+        'Unlimited Users',
+        'See Your Customers That Are Moving',
+        'See Your Customers That Have Recently Moved',
+        'Daily Email Updates',
+        'CSV Downloader',
+      ],
+      checkoutLink: 'https://buy.stripe.com/eVa7tU8cJgDs8UwdR4',
+    },
+    {
+      title: 'Gold',
+      description: 'For Large Businesses With An Extensive Client Base',
+      price: '$250',
+      benefits: [
+        '10,000 Clients',
+        'Unlimited Users',
+        'See Your Customers That Are Moving',
+        'See Your Customers That Have Recently Moved',
+        'Daily Email Updates',
+        'CSV Downloader',
+        'See Who Is Moving In Your Area Each Week',
+      ],
+      checkoutLink: 'https://buy.stripe.com/9AQ4hIcsZdrg9YA7sH',
+    },
+    {
+      title: 'Platinum',
+      description: 'For Long Established Client Bases',
+      price: '$400',
+      benefits: [
+        '20,000 Clients',
+        'Personalized Marketing Campaigns',
+        'Unlimited Users',
+        'See Your Customers That Are Moving',
+        'See Your Customers That Have Recently Moved',
+        'Daily Email Updates',
+        'CSV Downloader',
+        'See Who Is Moving In Your Area Each Week',
+      ],
+      checkoutLink: 'https://buy.stripe.com/9AQaG678F86W3Ac6os',
+    },
+    {
+      title: 'Enterprise',
+      description: 'Need more? We can help!',
+      price: '!!!',
+      benefits: ['Flexible Pricing Options', 'Custom Solutions For Your Needs', 'Setup done with you and your team'],
+      checkoutLink: 'https://ismycustomermoving.com/#contact',
+    },
+  ];
+
   return (
     <Wrapper>
-      <SectionTitle>Flexible pricing for agile teams</SectionTitle>
-      <AutofitGrid>
-        <PricingCard
-          title="free"
-          description="Give us a try for free"
-          benefits={['1 seat', '1 active project', 'Ulimited viewers', '10 blocks']}
+      <SectionTitle>Flexible pricing for any size business</SectionTitle>
+      <PricingContainer>
+        <ArrowButton onClick={() => setCurrentIndex((prevIndex) => Math.max(prevIndex - 1, 0))} disabled={currentIndex === 0}>
+          {'<'}
+        </ArrowButton>
+        <AutofitGrid>
+          {pricingPlans.slice(currentIndex, currentIndex + 3).map((plan, index) => (
+            <PricingCard
+              key={index}
+              title={plan.title}
+              description={plan.description}
+              benefits={plan.benefits}
+              checkoutLink={plan.checkoutLink}
+              isOutlined={index === 1}
+            >
+              {plan.price}
+            </PricingCard>
+          ))}
+        </AutofitGrid>
+        <ArrowButton
+          onClick={() => setCurrentIndex((prevIndex) => Math.min(prevIndex + 1, pricingPlans.length - 3))}
+          disabled={currentIndex === pricingPlans.length - 3}
         >
-          $0<span>/month</span>
-        </PricingCard>
-        <PricingCard
-          title="Starter"
-          description="Best for individual desginers"
-          benefits={['1 seat', '3 active project', 'Ulimited viewers', '100 blocks', 'CSV Downloader', 'Password protection']}
-          isOutlined
-        >
-          $29<span>/month</span>
-        </PricingCard>
-        <PricingCard
-          title="Premium"
-          description="Get your team together"
-          benefits={[
-            '10 seat',
-            '10 active project',
-            'Ulimited viewers',
-            'Unlimited blocks',
-            'CSV Downloader',
-            'Password protection',
-            'Customization',
-          ]}
-        >
-          $79<span>/month</span>
-        </PricingCard>
-      </AutofitGrid>
+          {'>'}
+        </ArrowButton>
+      </PricingContainer>
     </Wrapper>
   );
 }
@@ -48,4 +110,9 @@ const Wrapper = styled.div`
   & > *:not(:first-child) {
     margin-top: 8rem;
   }
+`;
+
+const PricingContainer = styled.div`
+  display: flex;
+  align-items: center;
 `;
