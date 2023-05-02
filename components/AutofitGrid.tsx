075b55dca6d0d26c61f4098cd11dfe2de7be8e06
reidelkins <reidelkins@outlook.com>
2023-05-02 15:21:08 -0500

    autofitgrid

--- components/AutofitGrid.tsx
+++ components/AutofitGrid.tsx
@@ -1,11 +1,20 @@
 import styled from 'styled-components';
 
+// const AutofitGrid = styled.div`
+//   --autofit-grid-item-size: 30rem;
+
+//   display: grid;
+//   grid-gap: 2rem;
+//   grid-template-columns: repeat(auto-fit, minmax(var(--autofit-grid-item-size), 1fr));
+//   margin: 0 auto;
+// `;
+
 const AutofitGrid = styled.div`
   --autofit-grid-item-size: 30rem;
 
-  display: grid;
-  grid-gap: 2rem;
-  grid-template-columns: repeat(auto-fit, minmax(var(--autofit-grid-item-size), 1fr));
+  display: flex;
+  justify-content: center;
+  gap: 2rem;
   margin: 0 auto;
 `;
 
