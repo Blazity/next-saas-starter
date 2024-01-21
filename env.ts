b6eabb395228f337b0310643dbc00e5004a31db3
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-01-21 08:56:49 +0000

    fix

--- env.ts
+++ env.ts
@@ -1,6 +1,6 @@
 export const EnvVars = {
-  SITE_NAME: 'concreteconstruction',
-  OG_IMAGES_URL: 'https://next-saas-starter-ashy.vercel.app/og-images/',
-  URL: 'https://next-saas-starter-ashy.vercel.app/',
+  SITE_NAME: 'purplewood-landscaping',
+  OG_IMAGES_URL: 'https://purplewood-landscaping.vercel.app/',
+  URL: 'https://purplewood-landscaping.vercel.app/',
   MAILCHIMP_SUBSCRIBE_URL: 'https://bstefanski.us5.list-manage.com/subscribe/post?u=66b4c22d5c726ae22da1dcb2e&id=679fb0eec9',
 };
